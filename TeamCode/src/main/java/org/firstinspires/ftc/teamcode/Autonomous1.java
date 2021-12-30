65e5eb86c1a9e8a217db44abaab971c65259c32a
karthik <77648774+pro-cactus@users.noreply.github.com>
2021-12-30 16:11:34 -0500

    Add files via upload

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous1.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/*
+link - https://learnroadrunner.com/quickstart-overview.html#are-you-using-drive-encoders
+ */
+
+@Autonomous(name="Red Ducks", group="SummerCamp")
+public class Autonomous1 extends LinearOpMode {
+    Robot robot;
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+
+    public void runOpMode() throws InterruptedException {
+
+        double[] p1 = {0, 0, 0, 0};
+        double[] p2 = {0, 0, 0, 0};
+        double[] p3 = {0, 0, 0, 0};
+
+
+
+
+
+    }
+}
