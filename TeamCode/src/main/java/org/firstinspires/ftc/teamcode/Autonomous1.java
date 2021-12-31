87a184d76b7e4775d8a92d07bd36c54e7964b535
karthikpalled <77648774+karthikpalled@users.noreply.github.com>
2021-12-31 14:33:10 -0500

    Add files via upload

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous1.java
@@ -24,23 +24,22 @@ public class Autonomous1 extends LinearOpMode {
         waitForStart();
         robot.Encoders();
 
-        robot.ElderWand((100)); // lower ElderWand
-        encoderDrive(0.3, 0.3,295,295,4); // move out
-        encoderTurn(0.3,-244,244,4); // turn
-        encoderDrive(-0.3, -0.3,-300,-300,4); // move back and maybe get team element
+        robot.ElderWand((300)); // lower ElderWand
+        encoderDrive(0.3, 0.3,295,295,10); // move out
+        encoderTurn(0.3,-244,244,10); // turn
+        encoderDrive(-0.1, -0.1,-300,-300,10); // move back and maybe get team element
         sleep(250); // stop briefly
-        encoderDrive(-0.3, -0.3, 850,850,4); // move forward to the end of the tile
+        encoderDrive(-0.1, -0.1, 850,850,10); // move forward to the end of the tile
         sleep(250); // stop briefly
         robot.ElderWand((2521)); // raise the Elder Wand
 
-        encoderDrive(0.1, 0.1, 50, 50, 4); // go forward again
-        encoderTurn(0.3,-190,190,4); // turn to face carousel
-        encoderDrive(0.3,0.3, 120,120,2); // go towards carousel
+        encoderDrive(0.2, 0.2, 150, 150, 10); // go forward again
+        encoderTurn(0.3,-145,145,10); // turn to face carousel
+        encoderDrive(0.3,0.3, 150,150,10); // go towards carousel
 
-        robot.setMotorPowers(0.1,0.1,0.1,0.1); // snug up the the carousel by time
-        sleep(1000);
+        robot.setMotorPowers(0.2,0.2,0.2,0.2); // snug up the the carousel by time
+        sleep(2000);
         robot.stop();
-
 
         //
         // swod
@@ -52,12 +51,12 @@ public class Autonomous1 extends LinearOpMode {
         sleep(500);
         robot.spinningWheelofDeath((0.7));
         sleep(1000);
+        robot.spinningWheelofDeath((0));
         //
-
-
-        encoderDrive(-0.4, -0.4, -1375,-1375,3); // back up to about the center of the field
-        encoderTurn(0.3,-313.19444444444, 313.19444444444, 4); // turn to go straight
-        encoderDrive(0.4, 0.4, 900, 900, 5);
+        encoderDrive(-0.4, -0.4, -1000,-100,5);
+        encoderDrive(-0.4, -0.4, -1470, -1470, 15); //
+        encoderTurn(0.3,-310, 310, 4); // turn to go straight //
+        encoderDrive(0.4, 0.4, 890, 890, 5);
         robot.DepositorM.setPosition(0.35);
         sleep(500);
         robot.DepositorS.setPosition(0.657);
@@ -65,8 +64,12 @@ public class Autonomous1 extends LinearOpMode {
         robot.DepositorS.setPosition(0.1);
         sleep(500);
         robot.DepositorM.setPosition(0.793650790794);
+        encoderTurnR(0.3, 145, -145, 5);
+        encoderDrive(1, 1, 930, 930, 5);
+        encoderTurnR(0.5,160, -160, 5);
+        encoderDrive(0.55, 0.55, 80, 80, 5);
 
-
+        
 
     }
 
@@ -169,12 +172,6 @@ public class Autonomous1 extends LinearOpMode {
         robot.frontLeft.setPower((-Math.abs(speed)));
         robot.backLeft.setPower((-Math.abs(speed)));
 
-        // keep looping while we are still active, and there is time left, and both motors are running.
-        // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
-        // its target position, the motion will stop.  This is "safer" in the event that the robot will
-        // always end the motion as soon as possible.
-        // However, if you require that BOTH motors have finished their moves before the robot continues
-        // onto the next step, use (isBusy() || isBusy()) in the loop test.
         while (opModeIsActive() &&
                 (runtime.seconds() < timeoutS) &&
                 (robot.frontLeft.isBusy() && robot.frontRight.isBusy())) {
@@ -218,15 +215,11 @@ public class Autonomous1 extends LinearOpMode {
         robot.backLeft.setTargetPosition((newLeftTarget ));
         robot.frontRight.setTargetPosition(newRightTarget);
         robot.backRight.setTargetPosition(newRightTarget);
-
-
         // Turn On RUN_TO_POSITION
         robot.frontLeft.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
         robot.backRight.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
         robot.backLeft.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
         robot.frontRight.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-
-
         // reset the timeout time and start motion.
         runtime.reset();
         robot.frontRight.setPower(-Math.abs(speed));
@@ -234,12 +227,6 @@ public class Autonomous1 extends LinearOpMode {
         robot.frontLeft.setPower((Math.abs(speed)));
         robot.backLeft.setPower((Math.abs(speed)));
 
-        // keep looping while we are still active, and there is time left, and both motors are running.
-        // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
-        // its target position, the motion will stop.  This is "safer" in the event that the robot will
-        // always end the motion as soon as possible.
-        // However, if you require that BOTH motors have finished their moves before the robot continues
-        // onto the next step, use (isBusy() || isBusy()) in the loop test.
         while (opModeIsActive() &&
                 (runtime.seconds() < timeoutS) &&
                 (robot.frontLeft.isBusy() && robot.frontRight.isBusy())) {
