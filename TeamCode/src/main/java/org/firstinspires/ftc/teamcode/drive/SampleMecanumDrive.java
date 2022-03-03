1b2c8b9b18a948abdae4952f426567c7b13774a9
prishadargan <94086244+prishadargan@users.noreply.github.com>
2022-03-02 19:52:54 -0500

    Update SampleMecanumDrive.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
@@ -53,8 +53,8 @@ import static org.firstinspires.ftc.teamcode.drive.DriveConstants.kV;
  */
 @Config
 public class SampleMecanumDrive extends MecanumDrive {
-    public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(0, 1, 3);
-    public static PIDCoefficients HEADING_PID = new PIDCoefficients(0, 1, 2.4);
+    public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(0, 0, 8);
+    public static PIDCoefficients HEADING_PID = new PIDCoefficients(0, 0, 7.8);
 
     public static double LATERAL_MULTIPLIER = 1.47;
 
