ebf2e6bd1afe0479645949d1d192a8eb404bc803
prishadargan <94086244+prishadargan@users.noreply.github.com>
2022-03-02 19:51:11 -0500

    Update DriveConstants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -46,7 +46,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 1.8897637795275593; // in
     public static double GEAR_RATIO = 0.0752086652120468; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 12.05; // in
+    public static double TRACK_WIDTH = 11.76; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -54,9 +54,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.001;
-    public static double kA = 0.00045;
-    public static double kStatic = 0.0013;
+    public static double kV = 0.00135;
+    public static double kA = 0.005;
+    public static double kStatic = 0.00048;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
@@ -100,8 +100,8 @@ public class DriveConstants {
      */
     public static double MAX_VEL = 40;
     public static double MAX_ACCEL = 40;
-    public static double MAX_ANG_VEL = Math.toRadians(270);
-    public static double MAX_ANG_ACCEL = Math.toRadians(270);
+    public static double MAX_ANG_VEL = 4.71238898038469;
+    public static double MAX_ANG_ACCEL = 4.71238898038469;
 
 
     public static double encoderTicksToInches(double ticks) {
